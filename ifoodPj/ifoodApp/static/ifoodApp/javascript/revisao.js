a79809b4426779fed62d0a31f393f1367f514a81
Loukraasz <peixotolukas06@gmail.com>
2024-04-15 23:02:47 -0300

    ...

---
+++ ifoodPj/ifoodApp/static/ifoodApp/javascript/revisao.js
@@ -0,0 +1,11 @@
+function teste(){
+   let x = document.getElementById("pag").value
+   console.log(x)
+   if (x == "Pix"){
+      img.style.display = "block";
+   }
+   else {
+      img.style.display = "none";
+   }
+        
+}
